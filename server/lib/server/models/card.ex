8cc9b59d6216a1af19fcc3cade130618af1c8063
Sandesh Soni <hello@sandeshsoni.com>
2019-09-21 13:23:04 +0530

    modify card

--- server/lib/server/models/card.ex
+++ server/lib/server/models/card.ex
@@ -1,4 +1,18 @@
 defmodule Server.Models.Card do
   import Faker
-  defstruct id: 0000, data: "Faker.Lorem.paragraph", title: "Faker.Lorem.sentence"
+
+  defstruct [id: 0000,
+             data: "",
+             title: "",
+             index: 0
+            ]
+
+  def new do
+    %__MODULE__{
+      id: Faker.UUID.v4,
+      data: Faker.Lorem.paragraph,
+      title: Faker.Lorem.sentence,
+    }
+  end
+
 end
