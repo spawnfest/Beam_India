be28645f5f164cabb705bf4a2c249fb000a8ce76
Sandesh Soni <hello@sandeshsoni.com>
2019-09-21 23:24:04 +0530

    newly nested code

--- server/lib/server_web/live/kanban_live/index.ex
+++ server/lib/server_web/live/kanban_live/index.ex
@@ -6,22 +6,39 @@ defmodule ServerWeb.KanbanLive.Index do
 
 
   def mount(_assigns, socket) do
-    %Board{columns: columns} = Board.new_random
+    board =  Board.new_random()
 
-    {:ok, assign(socket, :columns, columns )}
+    {:ok, assign(socket, :board, board )}
   end
 
   def render(assigns) do
     KanbanView.render("index.html", assigns)
   end
 
-  def handle_event("move-card", card_id, socket) do
+  def handle_event("move-card", %{"drag_column_id" => drag_column_id,
+                                  "drag_card_id" => drag_card_id,
+                                  "drop_card_id" => drop_card_id,
+                                  "drop_column_id" => drop_column_id
+                                 } = card_ids,
+    socket) do
 
+    board = socket.assigns.board
+
+    {drag_card, rest_of_board} =  pop_in(board.columns,[drag_column_id, drag_card_id])
+    drop_card  =  get_in(board.columns,[drop_column_id, drop_card_id])
+    # IO.puts inspect(socket.assigns.board)
     IO.puts "-------------------------"
-    IO.puts inspect(card_id)
+    IO.puts inspect(drag_card)
+    IO.puts inspect(drop_card)
     IO.puts "-------------------------"
+
+    # require IEx; IEx.pry
 
     {:noreply, socket}
+  end
+
+  def move_card do
+    #
   end
 
 end
